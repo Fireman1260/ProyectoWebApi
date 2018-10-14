bed72de8d5870df76607a41274db8f71a456499e
Fireman1260 <kev.villatoro1260@outlook.com>
2018-10-14 09:03:10 -0600

    Api Update finalizado

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var url = 'mongodb://localhost:27017/Heroes'
 
 app.use(express.json())
 
-var lastID = 9;
-
 app.get('/api/heroes', (req, res) => {
     listado  = []
     mongo.connect(url, function(err, db){
@@ -95,12 +93,6 @@ app.post('/api/heroes', (req, res) => {
 
 
 app.put('/api/heroes/:id', (req, res) => {
-    const heroe = listado.find(h => h.id === parseInt(req.params.id))
-    if(!heroe) {
-        res.status(404).send('El heroe con el ID brindado no se encontro')
-        return;
-    }
-        
     
     if(!req.body.name){
         res.status(400).send("Se requiere que el campo 'name' exista y lleve algun valor");
@@ -122,13 +114,31 @@ app.put('/api/heroes/:id', (req, res) => {
         res.status(400).send("Se requiere que el campo 'src' exista y lleve algun valor");
         return;
     }
-    heroe.name = req.body.name;
-    heroe.hero_typev = req.body.hero_type;
-    heroe.alter_ego = req.body.alter_ego;
-    heroe.species = req.body.species;
-    heroe.src ; req.body.src;
+    
 
-    res.status(204).send("Heroe modificado exitosamente")
+    mongo.connect(url, function(err, db){
+        const HeroDatabase = db.db('HeroDatabase');
+        var cursor = HeroDatabase.collection('Heroes').find({id: parseInt(req.params.id)});
+        cursor.next(function(err, doc) {
+            if (doc) {
+                HeroDatabase.collection('Heroes').updateOne({id: parseInt(req.params.id)}, { $set:{
+                    name: req.body.name,
+                    hero_type: req.body.hero_type,
+                    alter_ego: req.body.alter_ego,
+                    species: req.body.species,
+                    src: req.body.src
+                }},function(err, result){
+                    res.status(204).send('Heroe actualizado exitosamente')
+                })
+            }
+            else{
+                res.status(404).send('El heroe con el ID brindado no se encontro')
+            }
+        });
+        
+    });
+
+    
 });
 
 app.delete('/api/heroes/:id', (req, res) => {
